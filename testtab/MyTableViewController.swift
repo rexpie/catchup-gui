5e31a95b2029ae69628a1bfa8ec0716a17a64a5d
rexpie <rexpie@gmail.com>
2014-09-10 21:59:40 +0800

    api call

--- testtab/MyTableViewController.swift
+++ testtab/MyTableViewController.swift
@@ -8,15 +8,19 @@
 
 import UIKit
 
-class MyTableViewController: UIViewController , UITableViewDataSource, UITableViewDelegate {
+class MyTableViewController: UIViewController , UITableViewDataSource, UITableViewDelegate , APIControllerProtocol{
     
     @IBOutlet var appsTableView : UITableView?
     
     var tableData = []
     
+    var apiUtil = APIUtil()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        apiUtil.delegate = self
+        apiUtil.searchItunesFor("pocket")
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,19 +29,42 @@ class MyTableViewController: UIViewController , UITableViewDataSource, UITableVi
     }
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10;
+        return tableData.count
     }
     
     // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
     // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell    {
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
         
-        cell.textLabel?.text = "Row #\(indexPath.row)"
-        cell.detailTextLabel?.text = "Subtitle #\(indexPath.row)"
+        let rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
+        
+        cell.textLabel?.text = rowData["trackName"] as? String
+        
+        // Grab the artworkUrl60 key to get an image URL for the app's thumbnail
+        let urlString: NSString = rowData["artworkUrl60"] as NSString
+        let imgURL: NSURL = NSURL(string: urlString)
+        
+        // Download an NSData representation of the image at the URL
+        let imgData: NSData = NSData(contentsOfURL: imgURL)
+        cell.imageView?.image = UIImage(data: imgData)
+        
+        // Get the formatted price string for display in the subtitle
+        let formattedPrice: NSString = rowData["formattedPrice"] as NSString
+        
+        cell.detailTextLabel?.text = formattedPrice
         
         return cell
+    }
+    
+    func didReceiveAPIResults(results: NSDictionary)
+    {
+        var resultsArr: NSArray = results["results"] as NSArray
+        dispatch_async(dispatch_get_main_queue(), {
+            self.tableData = resultsArr
+            self.appsTableView!.reloadData()
+        })
     }
 
 }
