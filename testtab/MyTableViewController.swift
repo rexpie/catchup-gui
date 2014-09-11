bff2aec410d3dd1ab94fef826f851e4edeb03414
rexpie <rexpie@gmail.com>
2014-09-11 19:31:23 +0800

    update

--- testtab/MyTableViewController.swift
+++ testtab/MyTableViewController.swift
@@ -16,27 +16,33 @@ class MyTableViewController: UIViewController , UITableViewDataSource, UITableVi
     
     var apiUtil = APIUtil()
     
-    override func viewDidLoad() {
+    let kCellIdentifier: String = "SearchResultCell"
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         apiUtil.delegate = self
         apiUtil.searchItunesFor("pocket")
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
         return tableData.count
     }
     
     // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
     // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(kCellIdentifier) as UITableViewCell
         
         let rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
         
@@ -56,6 +62,20 @@ class MyTableViewController: UIViewController , UITableViewDataSource, UITableVi
         cell.detailTextLabel?.text = formattedPrice
         
         return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
+    {
+        var rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
+        
+        var name: String = rowData["trackName"] as String
+        var formattedPrice: String = rowData["formattedPrice"] as String
+        
+        var alert: UIAlertView = UIAlertView()
+        alert.title = name
+        alert.message = formattedPrice
+        alert.addButtonWithTitle("Ok")
+        alert.show()
     }
     
     func didReceiveAPIResults(results: NSDictionary)
