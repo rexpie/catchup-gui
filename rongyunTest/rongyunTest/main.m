9fbb1882809b2f1a25d37c55c74b4ef2ba8593ba
rexpie <rexpie@gmail.com>
2014-11-16 11:55:52 +0800

    ?

---
+++ rongyunTest/rongyunTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  rongyunTest
+//
+//  Created by 陈静 on 14-11-15.
+//  Copyright (c) 2014年 Team chatTime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
